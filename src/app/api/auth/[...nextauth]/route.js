d7b84f6974fa469c6535f260a21f6d4b8163a7c9
rastikawidiastuti <108380367+rastikawidiastuti@users.noreply.github.com>
2026-09-18 13:37:11 +0700

    finishing project

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -52,3 +52,20 @@ export const authOptions = {
                     role: token.role,
                 });
             }
+            return token;
+        },
+        async session({ session, token }) {
+            session.user.id = token.id;
+            session.user.user_id = token.user_id;
+            session.user.email = token.email;
+            session.user.role = token.role;
+            session.accessToken = token.accessToken;
+            return session;
+        },
+    },
+    pages: { signIn: "/login" },
+    secret: process.env.NEXTAUTH_SECRET,
+};
+
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
